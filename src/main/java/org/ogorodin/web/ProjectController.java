210e87795c8bdc26579ec430e20a0b1ed3f6ed92
Ig0r <siestitrokot@gmail.com>
2020-06-25 11:46:05 +0200

    Validation work - create new project Response

--- src/main/java/org/ogorodin/web/ProjectController.java
+++ src/main/java/org/ogorodin/web/ProjectController.java
@@ -1,10 +1,13 @@
 package org.ogorodin.web;
 
+import javax.validation.Valid;
+
 import org.ogorodin.domain.Project;
 import org.ogorodin.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +21,11 @@ public class ProjectController {
 	private ProjectService _projectService;
 	
 	@PostMapping("")
-	public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+	public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
+		
+		if(result.hasErrors()) {
+			return new ResponseEntity<String>("Invalid object", HttpStatus.BAD_REQUEST);
+		}
 		_projectService.saveOrUpdateProject(project);
 		return new ResponseEntity<Project>(project, HttpStatus.CREATED);
 	}
