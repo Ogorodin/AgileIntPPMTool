0a1b35361c0c5c2fc2ad029948a76b466b0d26c0
Ig0r <siestitrokot@gmail.com>
2020-06-26 23:11:19 +0200

    Dashboard, Navbar, ProjectItem - styled

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
+import ProjectItem from "./components/project/ProjectItem";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Dashboard />
+      <ProjectItem />
     </div>
   );
 }
