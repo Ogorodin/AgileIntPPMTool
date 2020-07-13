d505a91b2da70cba123ebb65e3bbb117f36fd1ed
Ig0r <siestitrokot@gmail.com>
2020-07-13 22:43:02 +0200

    Get project (redux only)
    + minor bug fix with the date

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,8 +1,17 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { getProjects } from "../actions/projectActions";
 
 import CreateProjectButton from "./project/CreateProjectButton";
 import ProjectItem from "./project/ProjectItem";
+import PropTypes from "prop-types";
+
 class Dashboard extends Component {
+  // lifecycle hook
+  componentDidMount() {
+    this.props.getProjects();
+  }
+
   render() {
     return (
       <div className="projects">
@@ -24,4 +33,13 @@ class Dashboard extends Component {
   }
 }
 
-export default Dashboard;
+Dashboard.propTypes = {
+  project: PropTypes.object.isRequired,
+  getProjects: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  project: state.project,
+});
+
+export default connect(mapStateToProps, { getProjects })(Dashboard);
